eb430f327ac2560c13cfc3f75bfa61abaee24db7
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-08-28 15:12:04 +0530

    Create getHumanChoice function in js

--- rps.js
+++ rps.js
@@ -1,4 +1,11 @@
 function getComputerChoice(){
     return Math.floor(Math.random()*3)
 }
-console.log(getComputerChoice())
+function getHumanChoice(){
+    switch(prompt("Write rock, paper or scissor")){
+        case "rock":return 0;
+        case "paper":return 1;
+        case "scissor":return 2;
+    }
+}
+console.log(getHumanChoice())
